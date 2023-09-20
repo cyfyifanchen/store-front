f80b90d1564721b5c6c727251f08fa6e49caee91
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-09-20 10:20:06 +0800

    Updates

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,11 +3,11 @@ const Footer = () => {
     <footer className="bg-white border-t">
       <div className="mx-auto py-10">
         <p className="text-center text-xs text-black">
-          &copy; 2023 Store, Inc. All rights reserved.
+          &copy; 2023 Elliott, Inc. All rights reserved.
         </p>
       </div>
     </footer>
   )
-};
+}
 
-export default Footer;
+export default Footer
