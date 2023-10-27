04de646d7f26096a1f9030abcfdc27078ae2abaf
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-10-27 19:20:43 +0800

    Adding changes to the billboard page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,27 +1,28 @@
-import getBillboard from "@/actions/get-billboard";
-import getProducts from "@/actions/get-products";
-import ProductList from "@/components/product-list";
-import Billboard from "@/components/ui/billboard";
-import Container from "@/components/ui/container";
+import getBillboard from '@/actions/get-billboard'
+import getProducts from '@/actions/get-products'
+import ProductList from '@/components/product-list'
+import Billboard from '@/components/ui/billboard'
+import Container from '@/components/ui/container'
 
-export const revalidate = 0;
+export const revalidate = 0
 
 const HomePage = async () => {
-  const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+  const products = await getProducts({ isFeatured: true })
+  const billboard = await getBillboard('52187b4a-60c0-4676-bed0-266c6b59a751')
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard 
-          data={billboard}
-        />
+        <Billboard data={billboard} />
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-          <ProductList title="Featured Products" items={products} />
+          <ProductList
+            title="Featured Products"
+            items={products}
+          />
         </div>
       </div>
     </Container>
   )
-};
+}
 
-export default HomePage;
+export default HomePage
--- components/ui/billboard.tsx
+++ components/ui/billboard.tsx
@@ -13,9 +13,7 @@ const Billboard: React.FC<BillboardProps> = ({ data }) => {
         className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover"
       >
         <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
-          <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
-            LOUISE LU REDEFINES FASHION
-          </div>
+          <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs"></div>
         </div>
       </div>
     </div>
